d3d18bf146768b28613d3ce334161baabb020d4c
msmi3128 <msmi3128@gmail.com>
2016-09-08 23:50:35 -0500

    main design complete. currently no dropdown in navigation menu

--- source_js/script.js
+++ source_js/script.js
@@ -1,1 +1,19 @@
-// Write any custom javascript functions here
+// /* Smooth scroll on home page */
+// $("a[href^='#/home']").click(function() {
+// 	console.log('this = ' + this);
+// 	console.log('location = ' + location);
+// 	console.log('location.pathname = ' + location.pathname);
+// 	console.log('location.hostname = ' + location.hostname);	
+// 	console.log('this.pathname = ' + this.pathname);
+// 	console.log('this.href= ' + this.href);
+//     if ( location.replace(/^\//,'') == this.replace(/^\//,'') || location.href == this.href ) {
+//         var target = $(this.hash);
+//         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+//            if (target.length) {
+//              $('html,body').animate({
+//                  scrollTop: target.offset().top - $('navlist').height()
+//             }, 700);
+//             return false;
+//         }
+//     }
+// });
